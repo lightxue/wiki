a5a90a26094db049a7c2a45e8b66f570b8ac16ab
lightxue <bkmgtp@gmail.com>
2014-05-20 16:37:51 +0000

    minor changes

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,20 +1,21 @@
-app_directory = "/home/lightxue/www/log/wiki"
+app_directory = File.expand_path(File.dirname(__FILE__))
+log_directory = "/home/lightxue/www/log/wiki"
 
 worker_processes 1
 
 working_directory app_directory
 
-listen "unix:#{app_directory}/unicorn/wiki.sock", :backlog => 1024
+listen "0.0.0.0:1313", :backlog => 1024
 
 timeout 60
 
-user 'lightxue', 'root'
+user 'www-data', 'www-data'
 
 File.umask(027)
 
 preload_app true
 
-pid "#{app_directory}/unicorn/wiki.pid"
+pid "#{log_directory}/unicorn/wiki.pid"
 
-stderr_path "#{app_directory}/unicorn/wiki.stderr.log"
-stdout_path "#{app_directory}/unicorn/wiki.stdout.log"
+stderr_path "#{log_directory}/unicorn/wiki.stderr.log"
+stdout_path "#{log_directory}/unicorn/wiki.stdout.log"
